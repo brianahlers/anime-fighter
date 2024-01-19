895e1cd8bcff48fc7aa5612340768cdb324a8683
Brian A <brian.ahlers@gmail.com>
2024-01-18 17:49:55 -0800

    game loop and display created. bg image displays

--- main.py
+++ main.py
@@ -2,22 +2,37 @@ import pygame
 
 pygame.init()
 
-window_width = 800
-window_height = 600
-window_title = "Anime Fighter"
+# create game window
+screen_width = 800
+screen_height = 600
+screen_title = "Anime Fighter"
 
-# Create the window
-game_window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption(window_title)
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption(screen_title)
 
-# Game loop
+# load bg image
+bg_image = pygame.image.load("./assets/images/matterhorn.jpg").convert_alpha()
+
+# function to draw bg
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (screen_width, screen_height))
+    screen.blit(scaled_bg, (0,0))
+
+# game loop
 running = True
 while running:
-    # Check for events
+
+    # draw bg
+    draw_bg()
+
+    # event handler
     for event in pygame.event.get():
         # Check if the user closed the window
         if event.type == pygame.QUIT:
             running = False
+
+    # update display
+    pygame.display.update()
 
     # handle player input
     keys = pygame.key.get_pressed()
